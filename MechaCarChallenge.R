4b24643d28abd632a2d02f8142a7e6fb07688a4b
renee tala <renee.salem2@gmail.com>
2022-04-12 12:57:09 -0500

    adding m15 files

---
+++ MechaCarChallenge.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(dplyr)
+mechacar_mpg <- read.csv(file='MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
+lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg, data=MechaCar_mpg)
+summary(lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg, data=MechaCar_mpg))
+
+suspension_coil <- read.csv(file='Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+total_summary <- suspension_coil %>% summarize(Mean =mean(PSI), Median =median(PSI), Variance = var(PSI), SD = sd(PSI))
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean =mean(PSI), Median =median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+t.test(suspension_coil$PSI, mu = 1500)
+
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
